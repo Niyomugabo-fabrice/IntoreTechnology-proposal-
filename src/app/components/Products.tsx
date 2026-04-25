b5f1f2abb306b82da8c3f8e57562cccfad532c21
Niyomugabo-fabrice <nfabrice058@gmail.com>
2026-04-25 13:16:33 +0200

    fixing the product links

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -69,7 +69,7 @@ export function Products() {
   };
 
   return (
-    <section className="py-20 bg-white">
+   <section id="products" className="py-20 bg-white">
       <div className="max-w-7xl mx-auto px-4 bg-gradient-to-br from-[#001D57] via-[#3b2a12] to-black 
                   shadow-[0_0_15px_rgba(176,141,87,0.4)] border border-[#B08D57]  p-2">
         <h2 className="text-3xl font-bold text-center mb-12 text-[#B08D57]">
